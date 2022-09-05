d8531c2d93dbaddf4e268c2d0bf0a32ab9063e66
gaeschpu <noal.gas@gmx.ch>
2022-09-05 09:48:20 +0200

    Hausaufgaben_2_9_2023

--- exercise/primzahlen.py
+++ exercise/primzahlen.py
@@ -1,6 +1,7 @@
 import numbers
 
-
 for index in range(2,100):
      if (range(2,100)%index) ==0:
-        print(numbers)
+        if True print("ist eine Primzahl")
+        if False print("ist keine Primzahl")
+    
