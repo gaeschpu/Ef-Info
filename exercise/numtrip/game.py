90a54ae581253696a2526582f3eea7a38bd373f9
gaeschpu <noal.gas@gmx.ch>
2022-09-16 16:35:53 +0200

    16.09.2022

---
+++ exercise/numtrip/game.py
@@ -0,0 +1,24 @@
+from re import X
+from turtle import*
+feld=[ 
+    [2,2,2,2,2],
+    [4,4,4,2,2],
+    [8,4,4,4,2],
+    [8,8,2,4,2],
+    [16,32,8,8,4],
+    ]
+
+    def
+for zeile in feld:
+    print('+------+------+------+------+------+')
+    print('|      |      |      |      |      |')
+    print(zeile)
+
+print (f'|  ',   (for i in zeile: [0][0]),   '  |')
+
+
+def rand():
+    print('+------+------+------+------+------+')
+
+def leerschlag():
+      
--- exercise/primzahlen.py
+++ exercise/primzahlen.py
@@ -1,11 +1,3 @@
 import numbers
-for index2 in range(2,100):
-   if (index2%index) ==0:
-      print numbers
 
-
-for index in range(2,100):
-     if (index2%index) ==0:
-        if True print("ist eine Primzahl")
-        else False print("ist keine Primzahl")
     
