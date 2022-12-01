6cb01591f57fc915d021dab7d1449114c72da5a4
gaeschp <noal.gas@gmx.ch>
2022-12-01 21:40:09 +0100

    WIP: Implementiere Benutzereingabe

--- numtrip/spielfeldversuch.py
+++ numtrip/spielfeldversuch.py
@@ -1,3 +1,4 @@
+1
 feld= [2,4,2,4,2],[4,2,8,8,8],[4,8,4,4,4],[2,4,4,8,8],[8,4,8,16,32]
 def spielfeld():
     zeilennummer= 1
@@ -55,7 +56,7 @@ def play():
     spielfeld()
     while True:
         x, y = eingabe()
-        process(x, y)
+        process[x, y]
         spielfeld()
 
 play()
