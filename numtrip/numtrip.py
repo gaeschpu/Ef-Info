d7b81cfa0a9ba0825d6f8d7da96c4df4a5de8808
gaeschp <noal.gas@gmx.ch>
2023-01-13 16:39:57 +0100

    Eingabeueberpruefung man so lange einngeben muss bis man wirklich eine gültige zahl eingibt

--- numtrip/numtrip.py
+++ numtrip/numtrip.py
@@ -1,5 +1,5 @@
 1
-feld= [2,4,2,4,2],[4,2,8,8,8],[4,8,4,4,4],[2,4,4,8,8],[8,4,8,16,32]
+feld= [[2,4,2,4,2],[4,2,8,8,8],[4,8,4,4,4],[2,4,4,8,8],[8,4,8,16,32]]
 def spielfeld():
     zeilennummer= 1
     """
@@ -48,12 +48,14 @@ def eingabe():
             x = int(x)
     except:
         print('Wissen sie nicht was eine Zahl ist?')
+        return False
 
     y = input('Welche Zeile soll ausgewählt werden? ')
     try:
         y = int(y)
     except:
         print('Wissen sie nicht was eine Zahl ist?')
+#   
     """""
     if y > 5: 
         print('Muss zwischen 1-5 sein!!!')
@@ -61,21 +63,35 @@ def eingabe():
     else:
         return y
 """
+#
     return (x - 1, y - 1)
 
+def aufdecken(zeile, spalte, zahl):
+# Rahmenbedingungen
+    if zeile < 0 or zeile > 4:
+        return False
+    if spalte < 0 or zeile> 4:
+        return False
+# Feldüberprüfung
+    if feld[zeile][spalte] == zahl:
+        feld[zeile][spalte] = - 1
+        aufdecken(zeile + 1, spalte, zahl) # unten .aufdecken(zeile - 1, spalte, zahl) # oben aufdecken(zeile, spalte + 1, zahl) # rechts aufdecken(zeile, spalte - 1, zahl) # links return True
+    else:
+        return False
 
 
 
 
 
+'''
 def process(col, row): # col= kollone row= spalte 
     feld[row][col] = 0 #auf dem spielfeld kollone[] spalte[] auf null setzen
-
+'''
 def play():
     spielfeld()
     while True:
         x, y = eingabe()
-        process(x, y) 
+        aufdecken(zeile, spalte, zahl)
         spielfeld()
 
 play()
